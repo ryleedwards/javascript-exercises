85340c06f5a89927e349c1fa8349d41dc5204138
Ryle Edwards <ryle.edwards@gmail.com>
2022-03-28 15:17:10 -0500

    Update 03 to completed state

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let original = string;
+  let strArray = [],
+    reversedArray = [];
+  for (i = 0; i < original.length; i++) {
+    strArray[i] = original.charAt(i);
+  }
+  reversedArray = strArray.reverse();
+  return reversedArray.join(``);
 };
 
 // Do not edit below this line
