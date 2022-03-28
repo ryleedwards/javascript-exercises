9e8702e5925464e75f131ae9c0b1c34319a09027
Ryle Edwards <ryle.edwards@gmail.com>
2022-03-28 17:22:33 -0500

    Add check for negative values, non-number values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,27 @@
 const sumAll = function (num1, num2) {
   let sum = 0;
-  let diff = Math.abs(num1 - num2);
-  console.log(diff);
-};
 
-sumAll(18, 14);
+  //check for negative values
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+
+  if (typeof num1 != "number" || typeof num2 != "number") {
+    return "ERROR";
+  }
+  if (num1 < num2) {
+    while (num1 <= num2) {
+      sum += num1;
+      num1++;
+    }
+  } else if (num1 > num2) {
+    while (num1 >= num2) {
+      sum += num1;
+      num1--;
+    }
+  }
+  return sum;
+};
 
 // Do not edit below this line
 module.exports = sumAll;
