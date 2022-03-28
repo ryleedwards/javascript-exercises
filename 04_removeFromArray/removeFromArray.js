7f3b017131963b617da79c17942a561d962e543a
Ryle Edwards <ryle.edwards@gmail.com>
2022-03-28 16:47:58 -0500

    Implement splice of array. Works on numbers

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,21 @@
 const removeFromArray = function (array, ...Args) {
-  console.log(array);
-  console.log(Args);
+  let returnedArray = array;
+
+  for (i = 0; i < Args.length; i++) {
+    // Evaluates size of ...Args and determines number of items to search for
+    let search = Args[i];
+    console.log(`search = ${search}`);
+    let index = returnedArray.indexOf(search);
+    console.log(index);
+    if (index > -1) {
+      // Uses Args[i].length in case the passed search item is > 1 character
+      returnedArray.splice(index, 1);
+    }
+  }
+  return returnedArray;
 };
 
-removeFromArray([1, 2, 3, 4], 3, 2, 5, 6);
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
