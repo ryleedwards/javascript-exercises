3676608c504247e1af4c9abff0b5ccf3c45c60f1
Ryle Edwards <ryle.edwards@gmail.com>
2022-03-28 14:50:49 -0500

    Complete 01, 02 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num >= 0) {
+    let returnedString = "";
+    for (i = 0; i < num; i++) {
+      returnedString += string;
+    }
+    return returnedString;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
