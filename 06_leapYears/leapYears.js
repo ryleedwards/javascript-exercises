abe896aeba97c877519a24e208fc0ae23abdc674
Ryle Edwards <ryle.edwards@gmail.com>
2022-03-28 17:33:04 -0500

    Complete exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // divisible by four hundred (leap year)
+  if (year % 400 == 0) {
+    return true;
+    // divisible by 100 (not leap year)
+  } else if (year % 100 == 0) {
+    return false;
+    // divisible by 4 (leap year)
+  } else if (year % 4 == 0) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
