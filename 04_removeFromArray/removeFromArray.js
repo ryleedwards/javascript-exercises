f62f4b5a809bec542237af14ef0bf4032e991092
Ryle Edwards <ryle.edwards@gmail.com>
2022-03-28 15:31:47 -0500

    Set up removeFromArray function w ...Args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...Args) {
+  console.log(array);
+  console.log(Args);
 };
+
+removeFromArray([1, 2, 3, 4], 3, 2, 5, 6);
 
 // Do not edit below this line
 module.exports = removeFromArray;
