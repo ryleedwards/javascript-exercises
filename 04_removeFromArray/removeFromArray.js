1341a5a5a872bc053b3b541dc038c1a8cae2093f
Ryle Edwards <ryle.edwards@gmail.com>
2022-03-28 16:53:29 -0500

    Finish exercise and Add comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,21 +1,19 @@
 const removeFromArray = function (array, ...Args) {
   let returnedArray = array;
-
+  // Evaluates size of ...Args and determines number of items to search for
   for (i = 0; i < Args.length; i++) {
-    // Evaluates size of ...Args and determines number of items to search for
+    //search is the element of Args[] for which we want to find in the array passed to removefromArray
     let search = Args[i];
-    console.log(`search = ${search}`);
+    // indexOf method used to find element that matches search variable
     let index = returnedArray.indexOf(search);
-    console.log(index);
+    // if it exists in the passed array, it will provide an index of 0, 1, 2 . . .
+    // if it does not exist, splice will not execute
     if (index > -1) {
-      // Uses Args[i].length in case the passed search item is > 1 character
       returnedArray.splice(index, 1);
     }
   }
   return returnedArray;
 };
-
-console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
