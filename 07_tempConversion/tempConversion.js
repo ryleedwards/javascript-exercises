4b112dbb2327ca045fe5026669ccd611fb8384d9
Ryle Edwards <ryle.edwards@gmail.com>
2022-03-28 17:52:12 -0500

    Solve exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function (tempF) {
+  let returnTemp = (tempF - 32) * (5 / 9);
+  return Math.round(returnTemp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (tempC) {
+  let returnTemp = tempC * (9 / 5) + 32;
+  return Math.round(returnTemp * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
+
+console.log(ftoc(32));
+console.log(ctof(32));
