6ad158bda1705db3a183099d4c9674f03224f319
Ryle Edwards <ryle.edwards@gmail.com>
2022-03-28 17:04:36 -0500

    Create diff variable

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  let diff = Math.abs(num1 - num2);
+  console.log(diff);
 };
+
+sumAll(18, 14);
 
 // Do not edit below this line
 module.exports = sumAll;
