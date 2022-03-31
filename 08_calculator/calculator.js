baf92e6576432444ab295a91d47bc27af1758e21
Ryle Edwards <ryle.edwards@gmail.com>
2022-03-31 18:10:53 -0500

    Complete 08-calc exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numArray) {
+  return numArray.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    0
+  );
 };
 
-const multiply = function() {
-
+const multiply = function (numArray) {
+  return numArray.reduce(
+    (previousValue, currentValue) => previousValue * currentValue
+  );
 };
 
-const power = function() {
-	
+const power = function (num, power) {
+  return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  //handle for if num = 0 . . . (0! = 1)
+  if (num == 0) {
+    return 1;
+  } else {
+    //create array of factorials incrementing down from init value
+    let factArr = [];
+    const numInitial = num;
+    for (i = 0; i < numInitial; i++) {
+      factArr[i] = num;
+      console.log(`i=${i} = ${factArr[i]}`);
+      num--;
+    }
+    //reduce array with multiplication
+    return factArr.reduce(
+      (previousValue, currentValue) => previousValue * currentValue
+    );
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +50,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
