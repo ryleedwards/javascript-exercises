e88ad6a6cf3caeb362680ec4cb38f8719dedfa76
Ryle Edwards <ryle.edwards@gmail.com>
2022-04-01 13:37:34 -0500

    Create function to determine age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,48 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
 
+  {
+    name: "John",
+    yearOfBirth: 1959,
+  },
+];
+
+const findTheOldest = function (people) {
+  people = determineAge(people);
+  return people[0].age;
 };
+
+function determineAge(people) {
+  people.forEach((person) => {
+    if (person.yearOfDeath) {
+      person.age = person.yearOfDeath - person.yearOfBirth;
+      //   //console.log(
+      //     `${person.name} died in ${person.yearOfDeath} at ${person.age}`
+      //   );
+    } else {
+      let currentYear = parseInt(new Date().getFullYear().toString());
+      person.age = currentYear - person.yearOfBirth;
+      //console.log(`${person.name} is still alive, and is ${person.age}`);
+    }
+  });
+  return people;
+}
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
