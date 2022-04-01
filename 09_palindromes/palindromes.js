33037537c4690cfed1053b2d7be20e0bbb1289e8
Ryle Edwards <ryle.edwards@gmail.com>
2022-03-31 20:27:29 -0500

    Complete 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  let arr = Array.from(sanitize(word));
+  let reversedArr = arr.slice();
+  reversedArr.reverse();
+  console.log(reversedArr);
+  console.log(arr);
+  const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+  return equals(reversedArr, arr);
 };
+
+console.log(palindromes("A car, a man, a maraca."));
+
+function sanitize(word) {
+  const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+  sanitized = word.replace(regex, "");
+  sanitized2 = sanitized.replace(/\s/g, "");
+  sanitized3 = sanitized2.toLowerCase();
+  return sanitized3;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
