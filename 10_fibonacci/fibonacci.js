f8f0c7ed0665a2b860c8c39a19ef1d00914a5aa6
Ryle Edwards <ryle.edwards@gmail.com>
2022-03-31 21:18:47 -0500

    Complete 10-fibonnaci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  // return x, y, n . . . from fibonacci sequence
+  let nParsed = parseInt(n);
+  if (nParsed > 0) {
+    let array = [1, 1];
+    if (nParsed > 1) {
+      for (i = 2; i < nParsed; i++) {
+        array[i] = array[i - 2] + array[i - 1];
+      }
+    }
+    return array[n - 1];
+  }
+  return "OOPS";
 };
+
+console.log(fibonacci(3));
 
 // Do not edit below this line
 module.exports = fibonacci;
