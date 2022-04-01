e7abd8ad63634253b9c4c9154cc0329305cee173
Ryle Edwards <ryle.edwards@gmail.com>
2022-04-01 15:52:23 -0500

    Added lessons learned

---
+++ 08_calculator/calc-solution-test.js
@@ -0,0 +1,14 @@
+const multiply = function (array) {
+  return array.length
+    ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+    : 0;
+};
+
+let array = [1, 3, 4, 5, 6, 7];
+let array2;
+
+if (array2) {
+  console.log("array has numbers");
+} else {
+  console.log("array empty");
+}
